4b42d9747b17c04775da4ee6f002b5ead05bd2d6
Bijay Thakur <bijaythakur330@gmail.com>
2022-09-20 15:49:29 +0545

    recipe plugin complete

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * Plugin Name: Recipe
+ * Description: A simple WordPress plugin that allows user to create recipes and rate those recipes
+ * Version: 1.0
+ * Author: Udemy
+ * Author URI: https://udemy.com
+ * Text Domain: recipe
+ */
+
+if( !function_exists( 'add_action' ) ){
+    echo "Hi there! I'm just a plugin, not much I can do when called directly.";
+    exit;
+}
+
+// Setup
+define( 'RECIPE_PLUGIN_URL', __FILE__ );
+
+// Includes
+include( 'includes/activate.php' );
+include( 'includes/init.php' );
+include( 'process/save-post.php' );
+include( 'process/filter-content.php' );
+include( 'includes/front/enqueue.php' );
+include( 'process/rate-recipe.php' );
+include( 'includes/admin/init.php' );
+include( 'blocks/enqueue.php' );
+include( dirname(RECIPE_PLUGIN_URL) . '/includes/widgets.php' );
+include( 'includes/widgets/daily-recipe.php' );
+include( 'includes/cron.php' );
+include( 'includes/deactivate.php' );
+include( 'includes/utility.php' );
+include( 'includes/shortcodes/creator.php' );
+include( 'process/submit-user-recipe.php' );
+include( 'includes/shortcodes/auth-form.php' );
+include( 'process/create-account.php' );
+include( 'process/login.php' );
+include( 'includes/shortcodes/auth-alt-form.php' );
+include( 'includes/front/logout-link.php' );
+include( 'includes/admin/dashboard-widgets.php' );
+include( 'includes/shortcodes/twitter-follow.php' );
+include( 'includes/admin/menus.php' );
+include( 'includes/admin/options-page.php' );
+include( 'process/save-options.php' );
+include( 'includes/admin/origin-fields.php' );
+include( 'process/save-origin.php' );
+include( 'includes/notice.php' );
+include( 'process/remove-notice.php' );
+include( 'includes/textdomain.php' );
+
+// Hooks
+register_activation_hook( __FILE__, 'r_activate_plugin' );
+register_deactivation_hook( __FILE__, 'r_deactivate_plugin' );
+add_action( 'init', 'recipe_init' );
+add_action( 'save_post_recipe', 'r_save_post_admin', 10, 3 );
+add_filter( 'the_content', 'r_filter_recipe_content' );
+add_action( 'wp_enqueue_scripts', 'r_enqueue_scripts', 100 );
+add_action( 'wp_ajax_r_rate_recipe', 'r_rate_recipe' );
+add_action( 'wp_ajax_nopriv_r_rate_recipe', 'r_rate_recipe' );
+add_action( 'admin_init', 'recipe_admin_init' );
+add_action( 'enqueue_block_editor_assets', 'r_enqueue_block_editor_assets' );
+add_action( 'enqueue_block_assets', 'r_enqueue_block_assets' );
+add_action( 'widgets_init', 'r_widgets_init' );
+add_action( 'r_daily_recipe_hook', 'r_daily_generate_recipe' );
+add_action( 'wp_ajax_r_submit_user_recipe', 'r_submit_user_recipe' );
+add_action( 'wp_ajax_nopriv_r_submit_user_recipe', 'r_submit_user_recipe' );
+add_action( 'wp_ajax_nopriv_recipe_create_account', 'recipe_create_account' );
+add_action( 'wp_ajax_nopriv_recipe_user_login', 'recipe_user_login' );
+// add_filter( 'authenticate', 'wp_authenticate_username_password', 20, 3 );
+// add_filter( 'authenticate', 'wp_authenticate_spam_check', 99 );
+// add_filter( 'authenticate', 'r_alt_authenticate', 100, 3 );
+add_filter( 'wp_nav_menu_secondary_items', 'ju_new_nav_menu_items', 999 );
+add_action( 'wp_dashboard_setup', 'r_dashboard_widgets' );
+add_action( 'admin_menu', 'r_admin_menus' );
+add_action( 'origin_add_form_fields', 'r_origin_add_form_fields' );
+add_action( 'origin_edit_form_fields', 'r_origin_edit_form_fields' );
+add_action( 'create_origin', 'r_save_origin_meta' );
+add_action( 'edited_origin', 'r_save_origin_meta' );
+add_action( 'admin_notices', 'r_admin_notices' );
+add_action( 'wp_ajax_r_dismiss_pending_recipe_notice', 'r_dismiss_pending_recipe_notice' );
+add_action( 'plugins_loaded', 'r_load_textdomain' );
+
+// Shortcodes
+add_shortcode( 'recipe_creator', 'r_recipe_creator_shortcode' );
+add_shortcode( 'recipe_auth_form', 'r_recipe_auth_form_shortcode' );
+// add_shortcode( 'recipe_auth_alt_form', 'r_recipe_auth_alt_form_shortcode' );
+add_shortcode( 'twitter_follow', 'r_twitter_follow_shortcode' );
---
+++ process/submit-user-recipe.php
@@ -0,0 +1,44 @@
+<?php
+
+function r_submit_user_recipe(){
+  $output                         = [ 'status' => 1 ];
+
+  if( empty($_POST['title']) ){
+    wp_send_json( $output );
+  }
+
+  global $wpdb;
+  $title                          =   sanitize_text_field( $_POST['title'] );
+  $content                        =   wp_kses_post( $_POST['content'] );
+  $recipe_data                    =   [];
+  $recipe_data['rating']          =   0;
+  $recipe_data['rating_count']    =   0;
+
+  $post_id                        =   wp_insert_post([
+    'post_content'                =>  $content,
+    'post_name'                   =>  $title,
+    'post_title'                  =>  $title,
+    'post_status'                 =>  'pending',
+    'post_type'                   =>  'recipe'
+  ]);
+
+  update_post_meta( $post_id, 'recipe_data', $recipe_data );
+
+  if(isset($_POST['attachment_id']) && !empty($_POST['attachment_id'])){
+    require_once( ABSPATH . 'wp-admin/includes/image.php' );
+    set_post_thumbnail( $post_id, absint($_POST['attachment_id']) );
+  }
+
+  // Update Recipe Pending Notice
+  $pending_recipe_count           =   $wpdb->get_var(
+    "SELECT COUNT(*) FROM `" . $wpdb->posts . "`
+    WHERE post_status='pending' AND post_type='recipe'"
+  );
+
+  if( $pending_recipe_count >= 5 ){
+    update_option( 'r_pending_recipe_notice', 1 );
+  }
+
+  $output['status']               =   2;
+  wp_send_json($output);
+}
